ae18d096ba82c21f9c8f08c068cf4ff6639ae9f2
lagou <nzolagoup.nguessan@aiesec.net>
2022-12-28 22:56:30 +0000

    my daily challenge on not bad

---
+++ js/dailychallenge.js
@@ -0,0 +1,54 @@
+let sentence = "The movie is not that bad, I like it";
+let wordNot = sentence.search("not")
+let wordBad  = sentence.search("bad")
+
+                        
+if (wordNot === -1) {
+
+    console.log(sentence)
+
+} else if ( wordNot < wordBad) {
+
+    console.log(wordNot)
+    console.log(wordBad)
+
+
+    let pullNot = sentence.slice(0, wordNot)
+    let pullBad = sentence.slice(wordBad + 3)
+    console.log(pullNot + "good" + pullBad)  
+
+ 
+} else { 
+  console.log(sentence)
+}
+
+
+////////////////////////////////////////////////
+
+// deuxieme methode avec les etapes
+
+// let sentence = "The movie is not that bad, I like it";
+// let wordNot = sentence.search("not")
+// let wordBad  = sentence.search("bad")
+
+                        
+// if ( wordNot > 0 && (wordNot  < wordBad)){
+// //    
+
+//     let toReplace = sentence.slice(wordNot,wordBad+3)
+//     let newSentence = sentence.replace(toReplace,"good" )
+//     console.log(toReplace)  
+//     console.log(sentence)  
+//     console.log(newSentence)  
+// }
+
+// else {
+//     console.log(sentence)
+// }
+
+
+
+
+
+
+
